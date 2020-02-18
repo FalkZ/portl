24d63f8962ac66f3c9972636fcff83d5c2411fb2
justaweb.dev <3829508-justaweb.dev@users.noreply.gitlab.com>
2020-02-18 17:20:08 +0100

    fixed relative path 2

--- portl-server.js
+++ portl-server.js
@@ -1,7 +1,8 @@
 import { ws } from "./ws.js";
-import { dirname, join } from "https://deno.land/std/path/mod.ts";
+import { join } from "https://deno.land/std/path/mod.ts";
 
-const file = join(dirname(import.meta.url), Deno.args[0]);
+const file = join("file:///", Deno.cwd(), Deno.args[0]);
+
 const toServe = await import(file);
 console.log("serving: ", file);
 
