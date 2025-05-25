bc123e0f00ae5a91f8bcada822baf416cd851407
Luca Bosin <luca.bosin@heviat.com>
2025-05-25 13:17:21 +0200

    fix: remove manual type declarations
    Since the previous commit fixes the automatic type generation, the manual type declarations are no longer needed.

--- src/lib/portal-state.svelte.ts
+++ src/lib/portal-state.svelte.ts
@@ -28,7 +28,13 @@ type PortalComponent = Component<{ children: ChildrenSnippet }, {}, "">;
 
 type ReceiverComponent = Component<{ placeholder?: ChildrenSnippet }, {}, "">;
 
-// always update /types/index.dts when changing the interface
+/**
+ * Creates a `Portal` and `Receiver` svelte component:
+ *
+ * `Portal` renders its child content at the location of the `Receiver`.
+ *
+ * @returns An object containing the Portal and Receiver components
+ */
 export const createPortal = (): {
     Receiver: ReceiverComponent;
     Portal: PortalComponent;
